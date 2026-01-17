672a94c0060c11f0ca2a1c59ac75be074c76e583
Micah Caunter <65076703+proc-gen@users.noreply.github.com>
2026-01-17 13:53:09 -0500

    feat: part 2 code (#1)

--- src/engine.ts
+++ src/engine.ts
@@ -1,18 +1,53 @@
-import { Display } from 'rot-js';
+import { Display } from 'rot-js'
+import { createWorld, addEntity, addComponent, type World, type EntityId } from 'bitecs'
+import { PositionComponent } from './ecs/components'
 
 export class Engine {
-  public static readonly WIDTH = 80;
-  public static readonly HEIGHT = 50;
+  public static readonly WIDTH = 80
+  public static readonly HEIGHT = 50
 
-  display: Display;
+  display: Display
+  world: World
+  player: EntityId
 
   constructor() {
-    this.display = new Display({width: Engine.WIDTH, height: Engine.HEIGHT});
+    this.display = new Display({ width: Engine.WIDTH, height: Engine.HEIGHT })
+    this.world = createWorld()
+
+    this.player = addEntity(this.world)
+    addComponent(this.world, this.player, PositionComponent)
+    PositionComponent.position[this.player] = { x: Engine.WIDTH / 2, y: Engine.HEIGHT / 2 }
+
+    window.addEventListener('keydown', (event) => {
+      this.update(event)
+    })
   }
 
   render() {
-    const x = Engine.WIDTH / 2;
-    const y = Engine.HEIGHT / 2;
-    this.display.draw(x, y, 'Welcome Adventurer', '#fff', '#000');
+    this.display.clear();
+    const position = PositionComponent.position[this.player]
+    this.display.draw(position.x, position.y, '@', '#fff', '#000')
   }
+
+  update(event: KeyboardEvent) {
+    event.preventDefault()
+    switch (event.key) {
+      case 'ArrowUp':
+        PositionComponent.position[this.player].y -= 1
+        break
+      case 'ArrowDown':
+        PositionComponent.position[this.player].y += 1
+        break
+      case 'ArrowLeft':
+        PositionComponent.position[this.player].x -= 1
+        break
+      case 'ArrowRight':
+        PositionComponent.position[this.player].x += 1
+        break
+    }
+
+    this.render()
+  }
+
+
 }
