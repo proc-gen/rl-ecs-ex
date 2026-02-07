b938c12a6f250be4a6c506887d128bd3210fa7c4
Micah Caunter <mcaunter12@gmail.com>
2026-02-06 19:40:07 -0500

    feat: deeper depth brings hardship

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 import { type Vector2 } from './vector-2'
 import { type Message } from './message'
 import { type HandleInputInfo } from './handleInputInfo'
+import { type WeightMap } from './weight-map'
 
-export type { Vector2, Message, HandleInputInfo }
+export type { Vector2, Message, HandleInputInfo, WeightMap }
